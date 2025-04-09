28e31f9e896103a99713247571ddaff0e8bdc2e9
Arun Raj <srvnarunraj@gmail.com>
2025-04-10 03:22:45 +0530

    Endpoint1: Get Products from DB

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 
 urlpatterns = [
+    path('getAll/',views.get_all_products, name='get_all_products'),
 ]
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from products.models import Product
 
 # Create your views here.
+def get_all_products(request):
+    data = Product.objects.first()
+    print(data.name)
+    return HttpResponse(data.name)
