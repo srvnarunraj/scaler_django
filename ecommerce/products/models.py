1b00d15a1feb9a0d64a6165d98b92599e44af0fe
Arun Raj <srvnarunraj@gmail.com>
2025-04-10 05:05:23 +0530

    Custom Primary Key Field

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+import uuid
 
 class AuditCols(models.Model):
     # created_at = models.DateTimeField(auto_now_add=True)
@@ -13,6 +13,7 @@ class AuditCols(models.Model):
         # The Fields will be used and this class will treat as abstractclass
 
 class Product(AuditCols):
+    pid = models.CharField(primary_key=True,default=f"POK_{uuid.uuid4().hex[:6].upper()}", max_length=10)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
