0bc54814720a13a1e06f8e6247a6494c5a832977
Arun Raj <srvnarunraj@gmail.com>
2025-04-10 04:58:38 +0530

    Abstract Class for Audit Cols

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.utils import timezone
 
-class Product(models.Model):
+
+class AuditCols(models.Model):
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        # This model will not create a table in the database 
+        # The Fields will be used and this class will treat as abstractclass
+
+class Product(AuditCols):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_available = models.BooleanField(default=True)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
+    
 
 
 Product.objects.all()
