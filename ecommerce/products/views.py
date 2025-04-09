ddec77bb80a627b6b992f932590ddca67d39b445
Arun Raj <srvnarunraj@gmail.com>
2025-04-10 03:31:51 +0530

    Get & Post Request for products

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -24,3 +24,23 @@ def get_products(request):
     products = Product.objects.all()
     serialized_products = ProductSerializer(products, many=True)
     return Response(serialized_products.data)
+
+@api_view(['GET'])
+def get_product_by_id(request, id):
+    try:
+        # product = Product.objects.filter(id=id) # In implementation it can return an empty list also 
+        product = Product.objects.get(id=id) # Will throw error if not found
+    except Product.DoesNotExist:
+        return Response({"error": "Product not found"}, status=404)
+    
+    serialized_product = ProductSerializer(product)
+    return Response(serialized_product.data)
+
+@api_view(['POST'])
+def post_product_by_id(request):
+    product = Product.objects.get(id=request.data.get('id'))
+    product.name = request.data.get('name' )
+    product.price = request.data.get('price')
+    product.save()
+    serialized_product = ProductSerializer(product)
+    return Response(serialized_product.data)
