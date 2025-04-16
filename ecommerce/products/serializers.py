a3bdec688966cea250afdf1ac916b3f2d6ac2161
Arun Raj <srvnarunraj@gmail.com>
2025-04-17 00:58:59 +0530

    New PK : Updated Serializers,models(pk gen)

--- ecommerce/products/serializers.py
+++ ecommerce/products/serializers.py
@@ -5,4 +5,4 @@ from products.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price']
+        fields = ['name', 'price','description']
